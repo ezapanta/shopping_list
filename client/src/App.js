8f0619b0686fe48574d09742c7e790dd23b02634
Edward <zapanta.ed@gmail.com>
2019-04-20 14:34:47 -0500

    We have redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import ShoppingList from './components/ShoppingList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import { Provider } from 'react-redux';
+import store from './Store'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
