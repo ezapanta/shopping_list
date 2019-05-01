79b9b9dec5e8a8d88d2051496085a2d0467f2544
Edward <zapanta.ed@gmail.com>
2019-04-30 20:33:43 -0500

    postbuild heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const items = require('./routes/api/items');
+const path = require('path');
 
 const app = express();
 
@@ -17,6 +18,13 @@ mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch( err => console.log(err));
+
+if(process.env.NODE_ENV == 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => { 
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // routes
 app.use('/api/items', items)
